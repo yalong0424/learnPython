8745e04de0ee0830b5e40e5ab08d8080e459081e
Jiming Wang (王继明) <Jiming.Wang@mediatek.com>
2017-07-07 17:25:15 +0800

    delete unittestdemo

--- unittest/UnitTestDemo/math/add.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-def add(x, y):
-    return x + y
--- unittest/UnitTestDemo/math/divide.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-def devide(x, y):
-    if (y == 0):
-        raise ZeroDivisionError()
-    else:
-        return x / y
